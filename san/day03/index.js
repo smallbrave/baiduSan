f419d67b2932eaca602f82660296f28880b7645a
smallbrave <1060401583@qq.com>
2019-11-24 10:27:09 +0800

    day03

---
+++ san/day03/index.js
@@ -0,0 +1,27 @@
+var MyApp = san.defineComponent({
+    template:`
+    <div>
+        <input type='text' placeholder='姓名（String）' value='{= name =}'>
+        <input type='text' placeholder='年龄（Number）' value='{= age =}'>
+        <input type='text' placeholder='简介（String）' value='{= des =}'>
+        <p><span>信息：</span><button on-click='clear()'>移出信息</button></p>
+        <p><span>姓名：</span><span>{{ name }}</span></p>
+        <p><span>年龄：</span><span>{{ age }}</span></p>
+        <p><span>简介：</span><span>{{ des }}</span></p>
+    </div>
+    `  ,
+    initData: function(){
+        return {
+            name: "",
+            age: "",
+            des: ""
+        }      
+    },
+    clear: function(){
+        this.data.set('name',"");
+        this.data.set('age',"");
+        this.data.set('des',"");
+   }
+})
+var myApp = new MyApp();
+myApp.attach(document.body);
