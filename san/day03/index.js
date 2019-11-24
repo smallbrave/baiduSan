a9aca4ed5bd45886616774fd4e4e0fb9d167fe25
smallbrave <1060401583@qq.com>
2019-11-24 10:32:18 +0800

    day03

--- san/day03/index.js
+++ san/day03/index.js
@@ -2,7 +2,7 @@ var MyApp = san.defineComponent({
     template:`
     <div>
         <input type='text' placeholder='姓名（String）' value='{= name =}'>
-        <input type='text' placeholder='年龄（Number）' value='{= age =}'>
+        <input type='number' placeholder='年龄（Number）' value='{= age =}'>
         <input type='text' placeholder='简介（String）' value='{= des =}'>
         <p><span>信息：</span><button on-click='clear()'>移出信息</button></p>
         <p><span>姓名：</span><span>{{ name }}</span></p>
