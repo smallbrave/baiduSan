7a08a1fa8dd40e653345b67179ee9fe870379c0f
smallbrave <1060401583@qq.com>
2019-12-03 20:15:00 +0800

    day11

---
+++ san/day11/src/main.js
@@ -0,0 +1,4 @@
+import san from 'san';
+import panel from './app.san'
+
+new panel().attach(document.body);
---
+++ san/day11/webpack.config.js
@@ -0,0 +1,31 @@
+module.exports={
+    entry: './src/main.js',
+    output:{
+        filename: 'bundle.js'
+    },
+    mode:'development',
+    devServer:{
+        publicPath:'/dist'
+    },
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:['style-loader','css-loader'],
+                exclude:/node_modules/,
+            },
+            {
+                test:/\.san$/,
+                use: 'san-loader',
+                exclude:/node_modules/
+            },
+            {
+                test:/\.js$/,
+                use:{
+                    loader:'babel-loader'
+                },
+                exclude:/node_modules/
+            }
+        ]
+    }
+}
